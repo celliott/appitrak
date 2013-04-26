c56aaff562cfc920a6f6f2cd1f211861656f5658
Chris Elliott <ctelliott@gmail.com>
2013-04-25 23:25:55 -0400

    fixed last week date range

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -5,8 +5,8 @@ module HabitsHelper
       @start_date = Time.now.beginning_of_week.to_date
       @end_date = Time.now.to_date
     elsif cookies[:trend] == '2'
-      @start_date = 1.week.ago.to_date
-      @end_date = Time.now.to_date
+      @start_date = 1.week.ago.beginning_of_week.to_date
+      @end_date = 1.week.ago.end_of_week.to_date
     elsif cookies[:trend] == '3'
       @start_date = 2.weeks.ago.to_date
       @end_date = Time.now.to_date
