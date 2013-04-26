de982e000090ad88b0bf3da2024c77fe09c83312
Chris Elliott <ctelliott@gmail.com>
2013-04-26 16:13:27 -0400

    timezone fix

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -1,9 +1,11 @@
+require 'date_extensions'
+
 module HabitsHelper
 
   def habits_chart_data    
     if cookies[:trend] == '1'
-      @start_date = Time.now.beginning_of_week.to_date
-      @end_date = Time.now.to_date
+      @start_date = Time.now.in_time_zone(cookies[:time_zone]).beginning_of_week.to_date
+      @end_date = Date.today_in_zone(cookies[:time_zone])
     elsif cookies[:trend] == '2'
       @start_date = 1.week.ago.beginning_of_week.to_date
       @end_date = 1.week.ago.end_of_week.to_date
