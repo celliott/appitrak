2ca5c24738dfd5597f8029caff69d9924911e00b
Chris Elliott <ctelliott@gmail.com>
2013-04-30 01:28:28 -0400

    added the start of forgot password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,16 @@ Appitrak::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+	config.action_mailer.smtp_settings = {
+	  :address              => "smtp.gmail.com",
+	  :port                 => 587,
+	  :domain               => 'cheapandsalty.com',
+	  :user_name            => 'habitak@cheapandsalty.com',
+	  :password             => 'Fulton19',
+	  :authentication       => 'plain',
+	  :enable_starttls_auto => true  }
 
   # Enable threaded mode
   # config.threadsafe!
