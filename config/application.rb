2d6f7d6c116ad4044edae71a9f572a220191c425
Chris Elliott <ctelliott@gmail.com>
2013-06-15 16:31:51 -0400

    removed line to config/application.rb for asset precompile error

--- config/application.rb
+++ config/application.rb
@@ -57,8 +57,6 @@ module Habitrak
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
-    
-    config.assets.initialize_on_precompile = true
+    config.assets.version = '1.0'    
   end
 end
